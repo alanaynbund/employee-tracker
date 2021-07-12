937a31714b1ad7f401e4bc8af1bfba159dd7107b
alanaynbund <82123716+alanaynbund@users.noreply.github.com>
2021-07-12 04:02:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Azsxdcfv01",
+    password: "",
     database: "employee_trackerDB"
 });
 
